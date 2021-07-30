bb1f5d793078458f2332b0cf774793b96a197893
zke <18638631337@163.com>
2021-07-30 19:16:11 +0800

    提交firstgit

---
+++ git-text/TextContectGit/src/com/zke/text/FirstGit.java
@@ -0,0 +1,8 @@
+package com.zke.text;
+
+public class FirstGit {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("contect git");
+    }
+}
